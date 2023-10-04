924a3e5c712e9380ffa73f72e033b7372dfa45b6
ViteKhan <victory_96@mail.ru>
2023-10-04 15:11:10 +0300

    added canvas to Canvas component and added styles for font

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,10 +1,36 @@
 import './Canvas.scss';
+import { observer } from 'mobx-react-lite';
+import { useEffect, useRef } from 'react';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../../constants';
+import Pencil from '../../models/Pencil';
+import canvasStore from '../../stores/CanvasStore';
+import toolStore from '../../stores/ToolStore';
+import { TCanvas } from '../../types';
 
 export const Canvas = () => {
+  const canvasRef = useRef<TCanvas>(null);
+
+  useEffect(() => {
+    canvasStore.setCanvas(canvasRef.current);
+    toolStore.setTool(new Pencil(canvasRef.current));
+  }, []);
+
+  const onMouseDown = () => {
+    if (canvasRef.current) {
+      canvasStore.pushToUndoList(canvasRef.current.toDataURL());
+    }
+  };
+
   return (
     <div className="canvasContainer">
-      <canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT}/>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
+      />
     </div>
   );
 };
+
+observer(Canvas);
