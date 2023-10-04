ae353ce54b3df3c115893545eb1e32754d745986
ViteKhan <victory_96@mail.ru>
2023-10-04 13:41:52 +0300

    added constants

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,9 +1,10 @@
 import './Canvas.scss';
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../../constants';
 
 export const Canvas = () => {
   return (
     <div className="canvasContainer">
-      canvas
+      <canvas width={CANVAS_WIDTH} height={CANVAS_HEIGHT}/>
     </div>
   );
 };
